ab73b3dccd6bd200aa898e4f9477ae4258686534
UsopKing7 <168589083+UsopKing7@users.noreply.github.com>
2024-07-06 02:55:43 -0400

    Update scan_the_ports.py

--- scan_the_ports.py
+++ scan_the_ports.py
@@ -6,15 +6,15 @@ scanner = nmap.PortScanner()
 scanner.scan('8.8.8.8', '1-1024', '-sV -sS -A -sC -O')
 
 for host in scanner.all_hosts():
-    print("host", host)
+    print("Host:", host)
     for proto in scanner[host].all_protocols():
         lport = scanner[host][proto].keys()
         for port in lport:
-            print("     port: ", port)
-            print("     name: ", scanner[host][proto][port]['name'])
-            print("     State: ", scanner[host][proto][port]['state'])
-            print("     versions: ", scanner[host][proto][port]['version'])
+            print("     Port:", port)
+            print("     Name:", scanner[host][proto][port]['name'])
+            print("     State:", scanner[host][proto][port]['state'])
+            print("     Version:", scanner[host][proto][port]['version'])
             if 'product' in scanner[host][proto][port]:
-                print("     product:", scanner[host][proto][port]['product'])
-            if 'extraInfo' in scanner[host][proto][port]:
-                print("     extraInfo: "[host][proto][port]['extraInfo'])
+                print("     Product:", scanner[host][proto][port]['product'])
+            if 'extrainfo' in scanner[host][proto][port]:
+                print("     Extra Info:", scanner[host][proto][port]['extrainfo'])

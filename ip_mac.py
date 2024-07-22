d019cb6fc482d4501ea1f09d21fe88fc1f208ae7
UsopKing7 <168589083+UsopKing7@users.noreply.github.com>
2024-07-22 17:21:57 -0400

    Update ip_mac.py

--- ip_mac.py
+++ ip_mac.py
@@ -1,4 +1,4 @@
-import nmap
+import nmap # type: ignore
 
 def mac_ip(ip_range):
     scanner = nmap.PortScanner()
@@ -6,14 +6,14 @@ def mac_ip(ip_range):
     scanner.scan(hosts = ip_range, arguments = "-sn")
 
     for host in scanner.all_hosts():
-        if 'mac' in host[host]['addresses']:
-            ipv4 = scanner[host]["addresses"]["ipv4"]
-            ipv6 = scanner[host]["addresses"]["ipv6"]
+        if 'mac' in scanner[host]['addresses']:
+            ipv4 = scanner[host]["addresses"].get('ipv4', 'N/A')
             mc = scanner[host]["addresses"]["mac"]
+            vendor = scanner[host]["vendor"].get(mc, "N/A")
 
             print(f"IPV4: {ipv4} ")
-            print(f"IPV6: {ipv6}")
             print(f"MAC: {mc} ")
-
+            print(f"VENDOR: {vendor}")
+            print("\n")
 
 mac_ip(ip_range = "192.168.1.0/24") 

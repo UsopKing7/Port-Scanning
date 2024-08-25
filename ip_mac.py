ccc1250b35e18daa1547fc5d4f4f6420ed535dfb
UsopKing7 <168589083+UsopKing7@users.noreply.github.com>
2024-08-25 02:05:02 -0400

    ipmac.py

--- ip_mac.py
+++ ip_mac.py
@@ -1,27 +1,63 @@
-import nmap # type: ignore
+import nmap  # type: ignore
+import argparse
+import concurrent.futures
 
-def mac_ip(ip_range):
-    
-    print(" _   _                 _  ___             ")
-    print("| | | |___  ___  _ __ | |/ (_)_ __   __ _ ")
-    print("| | | / __|/ _ \| '_ \| ' /| | '_ \ / _` |")
-    print("| |_| \__ \ (_) | |_) | . \| | | | | (_| |")
-    print(" \___/|___/\___/| .__/|_|\_\_|_| |_|\__, |")
-    print("                |_|                 |___/  \n")
-    
+# Colores ANSI
+RED = "\033[91m"
+RESET = "\033[0m"
+
+def banner():
+    print(f"{RED}  _    _     _    __  __          ")
+    print(" | |  | |   | |  |  \/  |         ")
+    print(" | |__| |_ _| | _| \  / | ___ _ __ ")
+    print(" |  __  / _` | |/ / |\/| |/ _ \ '__|")
+    print(" | |  | | (_| |   <| |  | |  __/ |   ")
+    print(" |_|  |_|\__,_|_|\_\_|  |_|\\___|_|   ")
+    print("                                   {RESET}\n")
+
+def scan_host(scanner, host):
+    scanner.scan(hosts=host, arguments="-sn")
+    result = []
+    if 'mac' in scanner[host]['addresses']:
+        ipv4 = scanner[host]["addresses"].get('ipv4', 'N/A')
+        mac = scanner[host]["addresses"]["mac"]
+        vendor = scanner[host]["vendor"].get(mac, "N/A")
+        result = [ipv4, mac, vendor]
+    return result
+
+def mac_ip(ip_range, threads, output_file):
     scanner = nmap.PortScanner()
+    results = []
 
-    scanner.scan(hosts = ip_range, arguments = "-sn")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        future_to_host = {executor.submit(scan_host, scanner, host): host for host in ip_range.split(',')}
+        for future in concurrent.futures.as_completed(future_to_host):
+            result = future.result()
+            if result:
+                results.append(result)
+                print(f"IPV4: {result[0]}")
+                print(f"MAC: {result[1]}")
+                print(f"VENDOR: {result[2]}")
+                print("\n")
 
-    for host in scanner.all_hosts():
-        if 'mac' in scanner[host]['addresses']:
-            ipv4 = scanner[host]["addresses"].get('ipv4', 'N/A')
-            mc = scanner[host]["addresses"]["mac"]
-            vendor = scanner[host]["vendor"].get(mc, "N/A")
+    if output_file:
+        with open(output_file, 'w') as file:
+            for result in results:
+                file.write(f"IPV4: {result[0]}\n")
+                file.write(f"MAC: {result[1]}\n")
+                file.write(f"VENDOR: {result[2]}\n")
+                file.write("\n")
 
-            print(f"IPV4: {ipv4} ")
-            print(f"MAC: {mc} ")
-            print(f"VENDOR: {vendor}")
-            print("\n")
+def main():
+    parser = argparse.ArgumentParser(description="IP and MAC Address Scanner")
+    parser.add_argument("-r", "--range", type=str, help="IP range to scan (e.g., 192.168.1.0/24)", required=True)
+    parser.add_argument("-t", "--threads", type=int, help="Number of threads to use for scanning", default=4)
+    parser.add_argument("-o", "--output", type=str, help="Output file to save results", default=None)
+    
+    args = parser.parse_args()
+    
+    banner()
+    mac_ip(args.range, args.threads, args.output)
 
-mac_ip(ip_range = "192.168.1.0/24") 
+if __name__ == "__main__":
+    main()
